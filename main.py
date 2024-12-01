bdbb418030201089a6a7766b8def288eed2887e6
Kim Joshua T. Lopez <145188219+KJTL290@users.noreply.github.com>
2024-12-01 18:09:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import tkinter as tk
 from tkinter import messagebox
 
+def login_action():
+    username = entry_username.get()
+    password = entry_password.get()
+    email = entry_email.get()
+
+    if username and password and email:
+        messagebox.showinfo("Log in Succesfully.", "Thank you for Choosing Our website.")
+    else:
+        messagebox.showerror("Error.", "Please Fill up the Form.")
+
 root = tk.Tk()
 root.title("Log In Form")
 
-label = tk.Label(root, text="Username")
-label.pack()
 
-label2 = tk.Label(root, text="Password")
-label2.pack()
 
-label3 = tk.Label(root, text="Email/Gmail")
-label3.pack()
+label_username = tk.Label(root, text="Username")
+label_username.pack()
+entry_username = tk.Entry(root, width=40)
+entry_username.pack()
 
-label4 = tk.Label(root, text="Log In")
-label4.pack()
+label_password = tk.Label(root, text="Password")
+label_password.pack()
+entry_password = tk.Entry(root, show="*", width=40)
+entry_password.pack()
+
+label_email = tk.Label(root, text="Email/Gmail")
+label_email.pack()
+entry_email = tk.Entry(root, width=40)
+entry_email.pack()
+login_button = tk.Button(root, text="Log In", command=login_action)
+login_button.pack()
 
 root.mainloop()
