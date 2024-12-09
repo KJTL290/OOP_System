1e55bf4fc476adf7f3611ab917c091a947afa878
Kim Joshua T. Lopez <145188219+KJTL290@users.noreply.github.com>
2024-12-09 08:59:30 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import mainform  
+import mainform
 
 def on_login():
     username = username_entry.get()
@@ -9,26 +9,26 @@ def on_login():
     root.destroy()
     mainform.open_main_form()  # Call the function to open the main form
 
-root = tk.Tk()
-root.title("Log In")
+if __name__ == "__main__":
+    # Main Window
+    root = tk.Tk()
+    root.title("Log In")
+    
+    # Frame for the login form
+    frame = tk.Frame(root, padx=50, pady=50, borderwidth=5)
+    frame.pack(padx=50, pady=50)
 
-frame = tk.Frame(root, padx=50, pady=50, borderwidth=5)
-frame.pack(padx=50, pady=50)
+    username_label = tk.Label(frame, text="Username:")
+    username_label.grid(row=1, column=0, sticky=tk.E, pady=5)
+    username_entry = tk.Entry(frame)
+    username_entry.grid(row=1, column=1, pady=5)
 
-library_label = tk.Label(frame, text="Library")
-library_label.grid(row=0, column=0, columnspan=2, pady=(0, 10))
+    email_label = tk.Label(frame, text="Email:")
+    email_label.grid(row=2, column=0, sticky=tk.E, pady=5)
+    email_entry = tk.Entry(frame)
+    email_entry.grid(row=2, column=1, pady=5)
 
-username_label = tk.Label(frame, text="Username:")
-username_label.grid(row=1, column=0, sticky=tk.E, pady=5)
-username_entry = tk.Entry(frame)
-username_entry.grid(row=1, column=1, pady=5)
+    login_button = tk.Button(frame, text="Log In", command=on_login)
+    login_button.grid(row=3, column=0, columnspan=2, pady=(10, 0))
 
-email_label = tk.Label(frame, text="Email:")
-email_label.grid(row=2, column=0, sticky=tk.E, pady=5)
-email_entry = tk.Entry(frame)
-email_entry.grid(row=2, column=1, pady=5)
-
-login_button = tk.Button(frame, text="Log In", command=on_login)
-login_button.grid(row=3, column=0, columnspan=2, pady=(10, 0))
-
-root.mainloop()
+    root.mainloop()
